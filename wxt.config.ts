0b6ce1560f4d9568cf3d0d069955e23fe53dcd89
acgotaku <acgotaku311@gmail.com>
2025-04-30 21:03:06 +0900

    feat: options page

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,28 @@
-import { defineConfig } from 'wxt';
+import { defineConfig, UserManifest } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  manifestVersion: 3,
+  manifest: env => {
+    const manifest: UserManifest = {
+      // languages https://developer.chrome.com/docs/extensions/reference/api/i18n?hl=zh-cn#locales
+      name: '__MSG_extName__',
+      description: '__MSG_extDescription__',
+      default_locale: 'en',
+      permissions: ['activeTab', 'storage'],
+      web_accessible_resources: [],
+      action: {
+        default_icon: {
+          '16': 'icon/16.png',
+          '32': 'icon/32.png',
+          '48': 'icon/48.png',
+          '96': 'icon/96.png',
+          '128': 'icon/128.png'
+        }
+      },
+      host_permissions: ['https://*/*', 'http://*/*']
+    };
+    return manifest;
+  }
 });
