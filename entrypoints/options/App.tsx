159e53dacf1c5605096468d4c02fd8855ab836d1
acgotaku <acgotaku311@gmail.com>
2025-05-01 08:28:08 +0900

    feat: router

--- entrypoints/options/App.tsx
+++ entrypoints/options/App.tsx
@@ -1,28 +1,12 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
-import './App.css';
+import { HashRouter } from 'react-router';
+import Routes from './routes';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App: React.FC = () => {
   return (
-    <>
-      <div></div>
-      <h1>WXT + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the WXT and React logos to learn more
-      </p>
-    </>
+    <HashRouter>
+      <Routes />
+    </HashRouter>
   );
-}
+};
 
 export default App;
