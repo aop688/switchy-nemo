13baa3fa37cfeb280f59013b89107da167095beb
acgotaku <acgotaku311@gmail.com>
2025-05-02 21:26:17 +0900

    fix: code

--- entrypoints/options/layouts/Sidebar/Sidebar.tsx
+++ entrypoints/options/layouts/Sidebar/Sidebar.tsx
@@ -1,22 +1,6 @@
-import { NavLink } from 'react-router';
-import type { LinkProps } from 'react-router';
 import { Menu } from '@/components';
 import { Settings, Save } from '@/assets/icons';
 import styles from './sidebar.module.css';
-
-// const Link = ({ children, to, ...props }: LinkProps) => {
-//   return (
-//     <NavLink
-//       className={({ isActive }) =>
-//         styles.link + (isActive ? ` ${styles.activated}` : '')
-//       }
-//       to={to}
-//       {...props}
-//     >
-//       {children}
-//     </NavLink>
-//   );
-// };
 
 const Sidebar = () => {
   return (
@@ -27,18 +11,13 @@ const Sidebar = () => {
         </div>
         <nav className={styles.nav}>
           <h2 className={styles.heading}>Settings</h2>
-          {/* <ol className={styles.navList}>
-            <li className={styles.navItem}>
-              <Link to="/general">General</Link>
-            </li>
-            <li className={styles.navItem}>
-              <Link to="/transfer">Import/Export</Link>
-            </li>
-          </ol> */}
           <Menu className={styles.menu}>
             <Menu.MenuItem label="General" to="general" Icon={Settings} />
             <Menu.MenuItem label="Import/Export" to="transfer" Icon={Save} />
           </Menu>
+        </nav>
+        <nav className={styles.nav}>
+          <h2 className={styles.heading}>Profiles</h2>
         </nav>
       </div>
     </aside>
