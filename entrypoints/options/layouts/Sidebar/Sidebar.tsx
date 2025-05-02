13587121a93f4cce39e16a3d4fc8f439cefbb9c9
acgotaku <acgotaku311@gmail.com>
2025-05-02 11:05:12 +0900

    feat: add menu

--- entrypoints/options/layouts/Sidebar/Sidebar.tsx
+++ entrypoints/options/layouts/Sidebar/Sidebar.tsx
@@ -1,4 +1,20 @@
+import { NavLink } from 'react-router';
+import type { LinkProps } from 'react-router';
 import styles from './sidebar.module.css';
+
+const Link = ({ children, to, ...props }: LinkProps) => {
+  return (
+    <NavLink
+      className={({ isActive }) =>
+        styles.link + (isActive ? ` ${styles.activated}` : '')
+      }
+      to={to}
+      {...props}
+    >
+      {children}
+    </NavLink>
+  );
+};
 
 const Sidebar = () => {
   return (
@@ -7,6 +23,17 @@ const Sidebar = () => {
         <div className={styles.logo}>
           <h1 className={styles.logoTitle}>Switchy Nimo</h1>
         </div>
+        <nav className={styles.nav}>
+          <h2 className={styles.heading}>Settings</h2>
+          <ol className={styles.navList}>
+            <li className={styles.navItem}>
+              <Link to="/">Home</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link to="/about">About</Link>
+            </li>
+          </ol>
+        </nav>
       </div>
     </aside>
   );
