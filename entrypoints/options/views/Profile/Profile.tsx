3c6252cdb4a26e61289d17f0314d60cf56747574
acgotaku <acgotaku311@gmail.com>
2025-05-06 13:49:12 +0900

    feat: remove profile

--- entrypoints/options/views/Profile/Profile.tsx
+++ entrypoints/options/views/Profile/Profile.tsx
@@ -1,14 +1,24 @@
-import { useParams } from 'react-router';
+import { useParams, useNavigate } from 'react-router';
 import { observer } from 'mobx-react-lite';
-import { Button } from '@/components';
+import { Button, Dialog } from '@/components';
 import { useStore } from '@options/stores';
 import styles from './profile.module.css';
 
 const ProfileView = observer(() => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const { profiles } = useStore();
+  const [showDeleteProfile, setShowDeleteProfile] = useState(false);
 
   const profile = profiles.getProfileById(id as string);
+
+  const deleteProfile = useCallback(() => {
+    if (profile) {
+      profiles.removeProfile(profile);
+      setShowDeleteProfile(false);
+      navigate('/');
+    }
+  }, [profile, profiles, navigate]);
 
   return (
     <div className={styles.profile}>
@@ -16,7 +26,36 @@ const ProfileView = observer(() => {
         <h1 className={styles.title}>Profile {profile?.name}</h1>
         <div className={styles.actions}>
           <Button>Save</Button>
-          <Button variant="filled-danger">Delete</Button>
+          <Button
+            variant="filled-danger"
+            onClick={() => setShowDeleteProfile(true)}
+          >
+            Delete
+          </Button>
+          <Dialog
+            visible={showDeleteProfile}
+            title={'Delete Profile'}
+            closable
+            onClose={() => setShowDeleteProfile(false)}
+            footer={
+              <div className={styles.deleteProfileFooter}>
+                <Button
+                  variant="outlined"
+                  onClick={() => setShowDeleteProfile(false)}
+                >
+                  Cancel
+                </Button>
+                <Button variant="filled-danger" onClick={deleteProfile}>
+                  Delete
+                </Button>
+              </div>
+            }
+          >
+            <div className={styles.deleteProfile}>
+              <p>Do you really want to delete the following profile?</p>
+              <p className={styles.deleteProfileName}>{profile?.name}</p>
+            </div>
+          </Dialog>
         </div>
       </header>
     </div>
