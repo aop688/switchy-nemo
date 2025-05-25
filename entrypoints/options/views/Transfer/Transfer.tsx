8a73acfa219517a865ea9cd629330db2cf1eb0e4
acgotaku <acgotaku311@gmail.com>
2025-05-25 23:08:06 +0900

    feat: settings ui

--- entrypoints/options/views/Transfer/Transfer.tsx
+++ entrypoints/options/views/Transfer/Transfer.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components';
 import styles from './transfer.module.css';
 
 const TransferView = () => {
@@ -6,6 +7,24 @@ const TransferView = () => {
       <header className={styles.header}>
         <h1 className={styles.title}>Import/Export</h1>
       </header>
+      <section className={styles.section}>
+        <h2 className={styles.sectionTitle}>Settings</h2>
+        <div className={styles.sectionBody}>
+          <div className={styles.sectionItem}>
+            <Button>Backup Settings</Button>
+            <p className={styles.sectionItemDescription}>
+              Make a full backup of your options (including profiles and all
+              other options).
+            </p>
+          </div>
+          <div className={styles.sectionItem}>
+            <Button variant="outlined">Restore Settings</Button>
+            <p className={styles.sectionItemDescription}>
+              Restore your Switchy Nemo options from a local file.
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
