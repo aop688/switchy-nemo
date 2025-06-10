415152da93c54fbd4db23bbd0a0eb3873f4d80bd
acgotaku <acgotaku311@gmail.com>
2025-06-10 15:40:59 +0900

    feat: popup view

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,34 +1,15 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
-import './App.css';
+import styles from './app.module.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>WXT + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <a href="./options.html" target="_blank">
-        open options page
+    <div className={styles.options}>
+      <button className={styles.optionsItem}>Direct</button>
+      <button className={styles.optionsItem}>System Proxy</button>
+      <hr className={styles.divider} />
+      <a href="./options.html" target="_blank" className={styles.optionsItem}>
+        Options
       </a>
-    </>
+    </div>
   );
 }
 
